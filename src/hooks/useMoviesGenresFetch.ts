a27b0e1920c9cfd7f62f2e26ba263fa500fc702e
Sachin Venugopalan <sachinvenugopalan99@gmail.com>
2025-04-30 20:00:00 +0530

    Fixed build failure issue

--- src/hooks/useMoviesGenresFetch.ts
+++ src/hooks/useMoviesGenresFetch.ts
@@ -4,11 +4,12 @@ import { apiSettings, Movies } from '../api/api';
 export const useMoviesGenresFetch = () => {
   const initialYear = 2012;
 
+
   // State for storing the list of items
   const [data, setData] = useState<any>([]);
   const [searchData, setSearchData] = useState<any>([]);
   const [genres, setGenres] = useState<any>([]);
-  const [activeTab, setActiveTab] = useState<any>('')
+  const [activeTab, setActiveTab] = useState<any> ('')
   // State for the current year offset
   const [yearOffset, setYearOffset] = useState(initialYear);
   // State for search and filter query params
@@ -16,50 +17,51 @@ export const useMoviesGenresFetch = () => {
 
   const currentYear = new Date().getFullYear();
 
-  const fetchGenres = useCallback(async() => {
+  const fetchGenres = async() => {
     const genresList: any = await apiSettings.fetchGenres();
+    console.log(genresList)
     setGenres(genresList?.genres)
-  }, []);
+
+  }
 
   useEffect(() => { 
     fetchGenres()
-  }, [fetchGenres])
+  }, [])
 
-  const fetchDataOnSearch = useCallback(async() => {
-    try {
-      const newData: Movies = await apiSettings.fetchMoviesonSearch(searchQuery);
-      setSearchData([...newData?.results]);
-    } catch(err) {
+  const fetchDataOnSearch = async() => {
+    try{
+    const newData: Movies = await apiSettings.fetchMoviesonSearch(searchQuery);
+     setSearchData([...newData?.results]);
+  } catch(err) {
       console.error(err);
     }
-  }, [searchQuery]);
+  }
 
-  const fetchData = useCallback(async (emptyList = false) => {
-    try {
-      const newData: Movies = await apiSettings.fetchMovies(emptyList ? initialYear : yearOffset, activeTab);
-      if (emptyList) {
-        setYearOffset(initialYear);
-      }
-      // Update the data state with the fetched data
-      const movies = emptyList ? [] : [...data];
-      if (!movies?.length) {
-        setData([{year: emptyList ? initialYear : yearOffset, movies:[...newData?.results] }])
-      } else {
-        const index = movies?.findIndex((item) => item?.year === yearOffset);
-        if (index < 0) {
-          if (yearOffset > movies?.[movies?.length - 1]?.year && yearOffset <= Number(currentYear)) {
-            setData([...movies, {year: yearOffset, movies:[...newData?.results]}])
-          }
-
-          if (yearOffset < movies?.[0]?.year) {
-            setData([{year: yearOffset, movies:[...newData?.results]},...movies])
-          }
+  const fetchData = async (emptyList = false) => {
+try{
+    const newData: Movies = await apiSettings.fetchMovies(emptyList ? initialYear :yearOffset, activeTab);
+  if (emptyList) {
+    setYearOffset(initialYear);
+  }
+    // Update the data state with the fetched data
+    const movies = emptyList ? [] : [...data];
+    if (!movies?.length) {
+      setData([{year: emptyList ? initialYear :yearOffset, movies:[...newData?.results] }])
+    } else {
+      const index = movies?.findIndex((item) => item?.year === yearOffset);
+      if (index < 0) {
+        if (yearOffset > movies?.[movies?.length - 1]?.year && yearOffset <= Number(currentYear)) {
+          setData([...movies, {year: yearOffset, movies:[...newData?.results]}])
         }
-      }
-    } catch(err) {
-      console.error(err);
+
+      if (yearOffset < movies?.[0]?.year) {
+          setData([{year: yearOffset, movies:[...newData?.results]},...movies])
     }
-  }, [yearOffset, activeTab, data, currentYear]);
+      }}
+      } catch(err) {
+      console.error(err);
+      }
+  };
 
   useEffect(() => {
     if (searchQuery) {
@@ -67,16 +69,16 @@ export const useMoviesGenresFetch = () => {
       fetchDataOnSearch()
     } else {
       setSearchData([]);
-      fetchData();
+  fetchData();
     }
-  }, [yearOffset, searchQuery, fetchData, fetchDataOnSearch]);
+}, [yearOffset, searchQuery]);
 
   useEffect(() => {
     setData([]);
     fetchData(true);
-  }, [activeTab, fetchData]);
+  },[activeTab])
 
-  useEffect(() => {
+      useEffect(() => {
     setData([]);
     setYearOffset(initialYear);
   }, [searchQuery]);
@@ -88,20 +90,21 @@ export const useMoviesGenresFetch = () => {
 
     // Check if the user has scrolled to the bottom of the page
     if (!searchQuery) {
-      if (scrollPosition >= documentHeight - 150 && yearOffset < Number(currentYear)) {
-        // Increment the year offset to fetch previous years
-        const year = data?.length ? data?.[data?.length - 1]?.year : yearOffset;
-        setYearOffset(year + 1);
-      }
-
-      // Check if the user has scrolled to the top of the page
-      if (window.scrollY === 0 && yearOffset > 0) {
-        const year = data?.length ? data?.[0]?.year: yearOffset;
-        // Decrement the year offset to fetch next years
-        setYearOffset(year - 1);
-      }
+    if (scrollPosition >= documentHeight - 150 && yearOffset < Number(currentYear)) {
+      // Increment the year offset to fetch previous years
+      const year = data?.length ? data?.[data?.length - 1]?.year : yearOffset;
+      setYearOffset(year + 1);
     }
-  }, [yearOffset, data, searchQuery, currentYear]);
+
+    // Check if the user has scrolled to the top of the page
+    if (window.scrollY === 0 && yearOffset > 0) {
+            const year = data?.length ? data?.[0]?.year: yearOffset;
+      // Decrement the year offset to fetch next years
+              setYearOffset(year - 1);
+    }
+    }
+
+  }, [yearOffset, data, searchQuery]);
 
   // Attach the scroll event listener
   useEffect(() => {
@@ -112,14 +115,7 @@ export const useMoviesGenresFetch = () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, [handleScroll]);
-
   return {
-    data: searchQuery ? searchData : data,
-    yearOffset,
-    setSearchQuery,
-    setActiveTab,
-    genres,
-    activeTab,
-    searchQuery
+    data : searchQuery? searchData: data, yearOffset, setSearchQuery, setActiveTab, genres, activeTab, searchQuery
   }
-};
+}
